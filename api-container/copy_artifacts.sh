28f4406841767b58ead5f9fd96b70be29efe51c7
yurimarca <yurimarca@gmail.com>
2025-03-30 15:10:38 -0300

    Trying simple AWS Lambda container API

--- api-container/copy_artifacts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create artifacts directory if it doesn't exist
-mkdir -p artifacts
-
-# Copy artifacts from the project root
-cp ../artifacts/*.joblib artifacts/
-
-echo "Artifacts copied successfully" 
