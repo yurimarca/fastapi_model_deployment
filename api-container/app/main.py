4815e3430ab2cc4d2cc2518b101a3d308d4728c9
yurimarca <yurimarca@gmail.com>
2025-03-30 17:08:39 -0300

    Trying again to make lambda api work

--- api-container/app/main.py
+++ api-container/app/main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-#app = FastAPI()
-app = FastAPI(
-    title="Test API",
-    description="Simple API for testing",
-    version="1.0.0",
-    root_path="/prod"
-)
+app = FastAPI()
 
 @app.get("/")
 def read_root():
